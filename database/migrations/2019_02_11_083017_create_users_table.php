189e3662fd0c80ace50e8ae2505928ab4eec8dec
deazth@gmail.com <deazth@gmail.com>
2019-02-14 11:38:25 +0800

    finalize user table

--- database/migrations/2019_02_11_083017_create_users_table.php
+++ database/migrations/2019_02_11_083017_create_users_table.php
@@ -18,17 +18,17 @@ class CreateUsersTable extends Migration
             $table->timestamps();
             $table->string('name');
             $table->string('email');
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
-            $table->string('staff_no', 10);
-            $table->integer('role');
-            $table->integer('curr_reserve');
-            $table->integer('curr_checkin');
-            $table->integer('last_checkin');
-            $table->string('mobile_no', 15);
-            $table->string('photo_url', 255);
-            $table->string('allowed_building', 255);
-            $table->integer('status');
+            $table->string('staff_id', 10);
+            $table->integer('role')->nullable();
+            $table->integer('curr_reserve')->nullable();
+            $table->integer('curr_checkin')->nullable();
+            $table->integer('last_checkin')->nullable();
+            $table->string('mobile_no', 15)->nullable();
+            $table->string('photo_url', 255)->nullable();
+            $table->string('allowed_building', 255)->nullable();
+            $table->integer('status')->nullable();
         });
     }
 
