0063e9c10a296c68771bcfdecef079504a174615
deazth@gmail.com <deazth@gmail.com>
2019-04-12 15:11:30 +0800

    https login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('login', [], false) }}">
                         @csrf
 
                         <div class="form-group row">
