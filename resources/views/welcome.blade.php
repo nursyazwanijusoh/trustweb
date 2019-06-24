e0736c80af2ae0746eee8c2a14d905b45fb152a6
deazth@gmail.com <deazth@gmail.com>
2019-06-24 14:56:16 +0800

    bootstrap caro welccome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,30 +56,44 @@
     </header><!--/header-->
 
     <section id="main-slider">
-        <div class="owl-carousel">
-            <div class="item" style="background-image: url('img/tm1.png');">
-                <div class="slider-inner">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div><!--/.item-->
-             <div class="item" style="background-image: url('img/tm2.jpg');">
-                <div class="slider-inner">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div><!--/.item-->
-        </div><!--/.owl-carousel-->
+      <div id="wpagecaro" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#wpagecaro" data-slide-to="0" class="active"></li>
+          <li data-target="#wpagecaro" data-slide-to="1"></li>
+          <li data-target="#wpagecaro" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="img/tm1.png" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Gambar 2</h5>
+              <p>menara TM</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/tm2.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Gambar 2</h5>
+              <p>helipad</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/tm3.jpg" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Gambar 3</h5>
+              <p>salam bukan muhrim</p>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#wpagecaro" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#wpagecaro" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     </section><!--/#main-slider-->
 
     <section id="cta" class="wow fadeIn">
