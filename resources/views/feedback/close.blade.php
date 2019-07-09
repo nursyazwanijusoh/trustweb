07bd5a7131148b067e05c7e51dd4c633ed0c0a55
deazth@gmail.com <deazth@gmail.com>
2019-07-09 16:40:04 +0800

    close feedback - changed from disabled to readonly

--- resources/views/feedback/close.blade.php
+++ resources/views/feedback/close.blade.php
@@ -14,25 +14,25 @@
                     <div class="form-group row">
                         <label for="ctc" class="col-md-4 col-form-label text-md-right">Email / Mobile No</label>
                         <div class="col-md-6">
-                            <input id="ctc" class="form-control" type="text" disabled value="{{ $contact }}">
+                            <input id="ctc" class="form-control" type="text" readonly value="{{ $contact }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
                         <div class="col-md-6">
-                            <input id="title" class="form-control" type="text" disabled value="{{ $feedback->title }}">
+                            <input id="title" class="form-control" type="text" readonly value="{{ $feedback->title }}">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="content" class="col-md-4 col-form-label text-md-right">Content</label>
                         <div class="col-md-6">
-                          <textarea rows="3" class="form-control" id="content" disabled>{{ $feedback->content }}</textarea>
+                          <textarea rows="3" class="form-control" id="content" readonly>{{ $feedback->content }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="agent" class="col-md-4 col-form-label text-md-right">Device Info</label>
                         <div class="col-md-6">
-                          <textarea rows="3" class="form-control" id="agent" disabled>{{ $feedback->agent }}</textarea>
+                          <textarea rows="3" class="form-control" id="agent" readonly>{{ $feedback->agent }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
