1e48cb1bc08afe3a5634d7a98f4c361b7cd0223e
deazth@gmail.com <deazth@gmail.com>
2019-07-22 14:21:48 +0800

    diff admin list type

--- resources/views/adminlist.blade.php
+++ resources/views/adminlist.blade.php
@@ -9,6 +9,7 @@
                 <div class="card-body">
                   <div class="card-columns">
                     @foreach($admins as $asub)
+                    @if($asub->role == 0)
                     <div class="card text-white bg-dark">
                       <div class="card-header text-center">{{ $asub['name'] }}</div>
                       <div class="card-body">
@@ -17,6 +18,16 @@
                         Mobile: {{ $asub['mobile_no'] }}</p>
                       </div>
                     </div>
+                    @else
+                    <div class="card bg-light">
+                      <div class="card-header text-center">{{ $asub['name'] }}</div>
+                      <div class="card-body">
+                        <p class="card-text">{{ $asub['unit'] }}<br />
+                        Email: <a href="mailto:{{ $asub['email'] }}">{{ $asub['email'] }}</a><br />
+                        Mobile: {{ $asub['mobile_no'] }}</p>
+                      </div>
+                    </div>
+                    @endif
                     @endforeach
                   </div>
                 </div>
