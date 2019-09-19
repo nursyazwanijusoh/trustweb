10df0009b4cd35b38727ba730c5d83aae77ddb85
deazth@gmail.com <deazth@gmail.com>
2019-09-19 09:33:11 +0800

    printer friendly generate all QR

--- resources/views/admin/genallqr.blade.php
+++ resources/views/admin/genallqr.blade.php
@@ -1,26 +1,28 @@
-@extends('layouts.app')
-
-@section('content')
-
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <!-- <div class="col-md-10"> -->
-            <div class="card">
-                <div class="card-header">{{ $location }}</div>
-                <div class="card-body">
-                  <form method="get" action="{{ route('admin.getallqr', [], false)}}">
-                    <input type="hidden" name="build_id" value="{{ $build_id }}"  />
-                    <div class="form-group row">
-                        <label for="width" class="col-md-4 col-form-label text-md-right">Size (px)</label>
-                        <div class="col-md-6">
-                            <input id="width" value="{{ $width }}" class="" type="number" name="width" required>
-                            <button type="submit" class="btn btn-primary">Regen</button>
-                        </div>
-                    </div>
-                  </form>
-                  <div class="d-flex flex-wrap">
-                    @foreach($seats as $seat)
-                    <div class="visible-print border text-center" >
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+  <!-- <main class="py-4">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="card">
+              <div class="card-header">{{ $location }}</div>
+              <div class="card-body"> -->
+                <form method="get" action="{{ route('admin.getallqr', [], false)}}">
+                  <input type="hidden" name="build_id" value="{{ $build_id }}"  />
+                  <div class="form-group row">
+                      <label for="width" class="col-md-4 col-form-label text-md-right">Size (px)</label>
+                      <div class="col-md-6">
+                          <input id="width" value="{{ $width }}" class="" type="number" name="width" required>
+                          <button type="submit" class="btn btn-primary">Regen</button>
+                      </div>
+                  </div>
+                </form>
+                <!-- <div class="d-flex flex-wrap"> -->
+                  @foreach($seats as $seat)
+                    <div class="visible-print border text-center" style="float:left; break-inside:avoid" >
                       <b>Scan to check in</b><br />
                       {!! QrCode::size($width)->margin(1)->generate('trUSt : ' . $seat['qr_code']); !!}
                       <p><u>{{ $seat['label'] }}</u></p>
@@ -28,11 +30,11 @@
                         <a href="https://trust.tm.com.my/info">https://trust.tm.com.my/info</a>
                       </p>
                     </div>
-                    @endforeach
-                  </div>
-                </div>
-            </div>
-        <!-- </div> -->
+                  @endforeach
+                <!-- </div> -->
+              <!-- </div>
+          </div>
+        </div>
     </div>
-</div>
-@endsection
+  </main> -->
+</body>
