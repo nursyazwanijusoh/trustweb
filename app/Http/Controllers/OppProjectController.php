d18c54fca28fe3cfcf4bf7f87fe7d072f76843dd
Afdzal <afdzal3@gmail.com>
2020-07-16 15:45:34 +0800

    Opp assigment table and project table

---
+++ app/Http/Controllers/OppProjectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OppProject;
+use Illuminate\Http\Request;
+
+class OppProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OppProject  $oppProject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OppProject $oppProject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OppProject  $oppProject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OppProject $oppProject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OppProject  $oppProject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OppProject $oppProject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OppProject  $oppProject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OppProject $oppProject)
+    {
+        //
+    }
+}
