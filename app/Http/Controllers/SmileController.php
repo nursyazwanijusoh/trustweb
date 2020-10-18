05efd3965c93b3cc9edc78ef321fa4b4b84fe2e1
Afdzal <afdzal3@gmail.com>
2020-10-18 21:39:24 +0800

    smile api

--- app/Http/Controllers/SmileController.php
+++ app/Http/Controllers/SmileController.php
@@ -12,15 +12,18 @@ use App\User;
 use DB;
 use App\HappyReason;
 use App\HappyType;
-
+use GuzzleHttp\Client;
 use Laravel\Passport\ClientRepository;
 use Laravel\Passport\Token;
 use Laravel\Passport\TokenRepository;
 
 class SmileController extends Controller
 {
+   
+    
     public function __construct()
     {
+        
         $this->middleware('auth');
 
     }
@@ -38,8 +41,26 @@ class SmileController extends Controller
     }
     public function submit(Request $req)
     {
-      dd($req);
-      return view('smile.index', []);
+        $api_uri = env('ERA_API_URI');
+
+        //dd($req);
+        $options = [
+             'json' =>[
+                'type' => $req->type,
+                'reason' => $req->reason,
+                'remark' => $req->remark,
+                'staffno'=>$req->user()->staff_no,
+                'source'=>"trUSt"
+            ]        
+        ];
+        //dd($options);
+        $reclient = new Client(["base_uri" => $api_uri]);
+        $request = $reclient->request('POST', '/api/happy/meter/external' .'?api_key='.env('ERA_API_KEY'), $options)->getBody()->getContents();
+      
+       
+        //return $request;
+        return view('smile.index', [ 'alert' => 'Hi. Thanks for contributing. Your input have been sent to ERA.']);
+     
     }
 
 
--- resources/views/smile/index.blade.php
+++ resources/views/smile/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <style>
-    .col20{
-        width: 20%;
-    }
+.col20 {
+    width: 20%;
+}
 
-    /* .row-eq-height {
+/* .row-eq-height {
    display: -webkit-box;
    display: -webkit-flex;
    display: -ms-flexbox;
@@ -14,57 +14,68 @@
    flex-wrap: wrap;
    padding-top:32px;
     } */
-    .smile_icon {
-    border-radius: 15px!important;
-    }
-    .v_happy {
-    background-color: #2ecc71!important;
-    }
-    .happy {
-    background-color: #00a6fb!important;
-    }
-    .neutral {
-    background-color: #DAA520!important;
-    }
-    .upset {
-        background-color: #f81!important;
-    }
-    .v_upset {
-        background-color: #ea2803!important;
-    }
-  .grow {
-      transition: .2s;
-  }
-  .grow:hover {
-    transform: scale(1.1);
-  }
-  .emoji_width {
+.smile_icon {
+    border-radius: 15px !important;
+}
 
-      box-shadow: 0 5px 10px 2px rgba(18,19,18,.19)!important;
-      border-radius: 50%!important;
-      width: 60px!important;
-      /* margin-top: 2rem!important; */
-  }
-  .minmax {
-      /* min-width: 60px!important;
+.v_happy {
+    background-color: #2ecc71 !important;
+}
+
+.happy {
+    background-color: #00a6fb !important;
+}
+
+.neutral {
+    background-color: #DAA520 !important;
+}
+
+.upset {
+    background-color: #f81 !important;
+}
+
+.v_upset {
+    background-color: #ea2803 !important;
+}
+
+.grow {
+    transition: .2s;
+}
+
+.grow:hover {
+    transform: scale(1.1);
+}
+
+.emoji_width {
+
+    box-shadow: 0 5px 10px 2px rgba(18, 19, 18, .19) !important;
+    border-radius: 50% !important;
+    width: 60px !important;
+    /* margin-top: 2rem!important; */
+}
+
+.minmax {
+    /* min-width: 60px!important;
       max-width: 400px!important;*/
-    min-width: 120px!important;
-    max-height: 230px!important;
-    border-radius: 15px!important;
-    margin-bottom: 9px!important;
-    margin-left: 10px!important;
-    margin-right:  10px!important;
+    min-width: 120px !important;
+    max-height: 230px !important;
+    border-radius: 15px !important;
+    margin-bottom: 9px !important;
+    margin-left: 10px !important;
+    margin-right: 10px !important;
     border-style: none;
-  }
-  .martext {
-    font-size: 20px!important;
-    font-weight: 500!important;
-    display: block!important;
-    margin-top: 12px!important;
-    margin-left: 5px!important;
-    margin-right: 5px!important;
-  }
-  /*
+}
+
+.martext {
+    font-size: 20px !important;
+    font-weight: 500 !important;
+    display: block !important;
+    margin-top: 12px !important;
+    margin-left: 5px !important;
+    margin-right: 5px !important;
+}
+
+/*
   .feel_text {
     font-size: 24px!important;
     font-weight: 500!important;
@@ -73,92 +84,102 @@
     margin-left: 20px!important;
     margin-right: 20px!important;
   }*/
-  .img {
-      vertical-align: middle;
-      border-style: none;
-  }
-
+.img {
+    vertical-align: middle;
+    border-style: none;
+}
 </style>
 
 <div class="container">
+    @if(isset($alert))
+    <div class="alert alert-info alert-dismissible">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        <strong>{{ $alert }}</strong>
+    </div>
+    @endif
     <div class="row justify-content-center">
-      <div class="text-center">
-        <div class="col-md-12"><br><br>
-          <h2>  HOW ARE YOU FEELING NOW? </h2>
-        </div>
-        <div >
-          <h6> <span style="border-bottom: 1px black solid; color:#00000063; ">Today, {{ date('d F, H:i A') }} </span></h6>
-        </div>
-        <br><br>
-        <!-- <div class="row row-eq-height"> -->
-        <div class="container">
-        <div class="card-deck ">
-          <div class="card text-white v_happy grow minmax" >
-            <a href="{{ route('smile.form', ['type' => 1], false) }}" class="text-white">
-            <div class="card-body " >
-            <div class="img">
-              <img class="emoji_width" src="/images/smile/v_happy.svg">
-            </div>
-            <div class="martext">
-              Very Happy
-            </div>
-            </div>
-            </a>
-          </div>
-          <!-- <div class="card text-white happy mb-5  grow smile_icon"> -->
-          <div class="card text-white happy grow minmax" >
-            <a href="{{ route('smile.form', ['type' => 2], false) }}" class="text-white">
-            <div class="card-body ">
-              <div class="img-line">
-                <img class="emoji_width" src="/images/smile/happy.svg" >
-              </div>
-              <div class="martext">
-                Happy
-              </div>
-            </div>
-            </a>
-          </div>
-          <div class="card text-white neutral grow minmax" >
-            <a href="{{ route('smile.form', ['type' => 3], false) }}" class="text-white">
-            <div class="card-body ">
-              <div class="img-line">
-                <img class="emoji_width" src="/images/smile/neutral.svg">
-              </div>
-              <div class="martext">
-                Neutral
-              </div>
-            </div>
-            </a>
-          </div>
-          <div class="card text-white upset grow minmax" >
-            <a href="{{ route('smile.form', ['type' => 4], false) }}" class="text-white">
-            <div class="card-body ">
-              <div class="img-line">
-                <img class="emoji_width" src="/images/smile/upset.svg">
-              </div>
-              <div class="martext">
-                Upset
-              </div>
-            </div>
-          </a>
-          </div>
-          <div class="card text-white v_upset grow minmax" >
-            <a href="{{ route('smile.form', ['type' => 5], false) }}" class="text-white">
-            <div class="card-body ">
-              <div class="img-line">
-                <img class="emoji_width" src="/images/smile/v_upset.svg">
-              </div>
-              <div class="martext">
-              <!-- <div class="feel_text"> -->
-                Very Upset
-              </div>
-            </div>
-            </a>
-          </div>
-        </div>
-        </div>
 
-        <!-- <div class="row justify-content-center">
+        <div class="text-center">
+
+            <div class="col-md-12">
+
+            
+                <h2> HOW ARE YOU FEELING NOW? </h2>
+            </div>
+            <div>
+                <h6> <span style="border-bottom: 1px black solid; color:#00000063; ">Today, {{ date('d F, H:i A') }}
+                    </span></h6>
+            </div>
+            <br><br>
+            <!-- <div class="row row-eq-height"> -->
+            <div class="container">
+                <div class="card-deck ">
+                    <div class="card text-white v_happy grow minmax">
+                        <a href="{{ route('smile.form', ['type' => 1], false) }}" class="text-white">
+                            <div class="card-body ">
+                                <div class="img">
+                                    <img class="emoji_width" src="/images/smile/v_happy.svg">
+                                </div>
+                                <div class="martext">
+                                    Very Happy
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- <div class="card text-white happy mb-5  grow smile_icon"> -->
+                    <div class="card text-white happy grow minmax">
+                        <a href="{{ route('smile.form', ['type' => 2], false) }}" class="text-white">
+                            <div class="card-body ">
+                                <div class="img-line">
+                                    <img class="emoji_width" src="/images/smile/happy.svg">
+                                </div>
+                                <div class="martext">
+                                    Happy
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card text-white neutral grow minmax">
+                        <a href="{{ route('smile.form', ['type' => 3], false) }}" class="text-white">
+                            <div class="card-body ">
+                                <div class="img-line">
+                                    <img class="emoji_width" src="/images/smile/neutral.svg">
+                                </div>
+                                <div class="martext">
+                                    Neutral
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card text-white upset grow minmax">
+                        <a href="{{ route('smile.form', ['type' => 4], false) }}" class="text-white">
+                            <div class="card-body ">
+                                <div class="img-line">
+                                    <img class="emoji_width" src="/images/smile/upset.svg">
+                                </div>
+                                <div class="martext">
+                                    Upset
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card text-white v_upset grow minmax">
+                        <a href="{{ route('smile.form', ['type' => 5], false) }}" class="text-white">
+                            <div class="card-body ">
+                                <div class="img-line">
+                                    <img class="emoji_width" src="/images/smile/v_upset.svg">
+                                </div>
+                                <div class="martext">
+                                    <!-- <div class="feel_text"> -->
+                                    Very Upset
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <div class="row justify-content-center">
           <div class="col-lg-2 col-md-12" style="background-color:lavender;">
               <a href="{{ route('smile.form', [], false) }}">
                 <div class="box box-solid">
@@ -175,8 +196,8 @@
               </a>
           </div>
         </div> -->
-        <!-- </div> -->
-      </div>
+            <!-- </div> -->
+        </div>
     </div>
 </div>
 @endsection
