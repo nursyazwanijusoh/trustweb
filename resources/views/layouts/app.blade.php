7e242a81101247ca737ff6d749cb68793b37ced4
Amer <deazth@gmail.com>
2020-10-19 11:41:11 +0800

    custom title for common pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} : @yield('title', Route::current()->uri()) </title>
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
