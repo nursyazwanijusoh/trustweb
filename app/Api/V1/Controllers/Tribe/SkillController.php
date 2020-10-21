0c040da6b77e9514dcf078c5120ead36570d6253
Afdzal <afdzal3@gmail.com>
2020-10-21 16:11:39 +0800

    WIP: usersbyskills

--- app/Api/V1/Controllers/Tribe/SkillController.php
+++ app/Api/V1/Controllers/Tribe/SkillController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Api\V1\Controllers\Tribe;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,47 +10,72 @@ use App\User;
 use App\CommonSkillset;
 use App\PersonalSkillset;
 
-
 class SkillController extends Controller
 {
-   public function getSkills(Request $req){
-
-      $skillset = CommonSkillset::all('id','name');
-      $competency = [
+    public function getSkills(Request $req)
+    {
+        $skillset = CommonSkillset::all('id', 'name');
+        $competency = [
          ['id'=> 1, 'descr'=>'noob'],
          ['id'=>2, 'descr'=>'soso'],
          ['id'=>3, 'descr'=>'expert']];
 
-      $result = [
+        $result = [
          'competency'=>$competency,
          'skillset'=>$skillset
          
       ];
 
 
-      return $result;
+        return $result;
     }
-    public function getUsersBySkills(Request $req){
-      $users = User::query();
-      $pss = PersonalSkillset::query();
+    public function getUsersBySkills(Request $req)
+    {
+        $users = User::query();
+        $pss = PersonalSkillset::query();
 
-      $skills = $req->skills;
-      foreach ($skills as $key => $value) {
+        $skills = $req->skills;
+        /*
+        foreach ($skills as $key => $value) {
          $users->whereIn('id', PersonalSkillset::where('common_skill_id', $value['skill'])->where('level', '=', $value['lvl'])->pluck('staff_id'));
-      }
-      //dd($value['skill']);
+     }
+     */
+
+        foreach ($skills as $key => $value) {
+            if ($value['rule'] == 'and') {
+                $users->where(function ($query) use ($value) {
+                    $query->whereIn(
+                        'id',
+                        PersonalSkillset::where('common_skill_id', $value['skill'])
+                    ->where('level', '=', $value['lvl'])
+                    ->pluck('staff_id')
+                    );
+                });
+            }
+
+            if ($value['rule'] == 'or') {
+                $users->whereOr(function ($query) use ($value) {
+                    $query->whereIn(
+                        'id',
+                        PersonalSkillset::where('common_skill_id', $value['skill'])
+                    ->where('level', '=', $value['lvl'])
+                    ->pluck('staff_id')
+                    );
+                });
+            }
+        }
+
+
+  
+
+
+        //dd($value['skill']);
      
-      //$users->whereIn('id', PersonalSkillset::where('common_skill_id', $value)->where('level', '!=', 0)->pluck('staff_id'));
+        //$users->whereIn('id', PersonalSkillset::where('common_skill_id', $value)->where('level', '!=', 0)->pluck('staff_id'));
 
-      $result = $users->get();
-      //dd($users->toSql());
+        $result = $users->get(['id']);
+        //dd($users->toSql());
     
-      return $result;
+        return $result;
     }
-
-
-
-
-
-
 }
--- routes/api.php
+++ routes/api.php
@@ -172,7 +172,7 @@ $api->version('v1', [
   'prefix' => 'api/tribe'
 ], function ($api) {  
   $api->post('/staffno',  ['as' => 'api.tribe.getDetails', 'uses' => 'App\Api\V1\Controllers\Tribe\UserController@getDetail']);
-  $api->get('/userbyskillset',  ['as' => 'api.tribe.userbyskillset', 'uses' => 'App\Api\V1\Controllers\Tribe\SkillController@getUsersBySkills']);
+ 
  
   
 });
@@ -181,4 +181,5 @@ $api->version('v1', [
 $api->version('v1', ['prefix' => 'api/tribe',], function ($api) {
   $api->get('/vt',  ['as' => 'api.tribe.vt', 'uses' => 'App\Api\V1\Controllers\Tribe\UserController@validateToken']);
   $api->get('/skillset',  ['as' => 'api.tribe.skillset', 'uses' => 'App\Api\V1\Controllers\Tribe\SkillController@getSkills']);
+  $api->get('/userbyskillset',  ['as' => 'api.tribe.userbyskillset', 'uses' => 'App\Api\V1\Controllers\Tribe\SkillController@getUsersBySkills']);
 });
